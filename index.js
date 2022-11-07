173e387dbc490d2dcb58b55ac16861675dd10ad9
Md Ikbal Hosen <mdiqbalhossain6463@gmail.com>
2022-11-07 22:37:16 +0600

    service api with id is added

--- index.js
+++ index.js
@@ -37,6 +37,15 @@ async function run() {
         })
 
 
+        // get a single service from the database
+        app.get('/services/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const service = await servicesCollection.findOne(query);
+            res.send(service);
+        })
+
+
 
 
 
